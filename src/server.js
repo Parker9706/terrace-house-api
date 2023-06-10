d79c0de843ff905ef7964e27b1fb83ffc52d4fe1
Parker Hutcheson <pdhutcheson@gmail.com>
2023-06-10 17:11:47 -0400

    Routes consolidated, adjustment to rate limiting

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import express from "express";
+import rateLimit from 'express-rate-limit';
+import router from "./routes.js";
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000,
+  message: 'You have made too many requests',
+});
+
+app.use(limiter);
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
+
+app.use((req, res, next) => {
+  return res.status(404).send('Endpoint not found...');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
