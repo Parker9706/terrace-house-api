dd9b871e4e4cfa4faf6e020be2593c21cd86a630
Parker Hutcheson <pdhutcheson@gmail.com>
2023-06-18 02:23:18 -0400

    Frontend changes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,18 @@
 import express from 'express';
-import { findAllMembers, searchForAMember } from "./controllers/members.js";
+import { findAllMembers, findSpecificMembersBasedOnCriteria } from "./controllers/members.js";
 import { getARandomQuote, getQuoteByMember, getASpecificQuote } from './controllers/quotes.js';
 
 const router = express.Router();
 
 // Members
 router.get('/members', async (req, res) => {
-  const { language } = req.params;
   const result = await findAllMembers();
   return res.status(200).json(result);
 });
 
-router.get('/member/:name', async (req, res) => {
-  const { name } = req.params;
-  const result = await searchForAMember(name);
+router.get('/members/:criteria', async (req, res) => {
+  const { criteria } = req.params;
+  const result = await findSpecificMembersBasedOnCriteria(criteria);
   return res.status(200).json(result);
 });
 
