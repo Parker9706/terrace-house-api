37f9a42c11a50be8b49ed10a12f312067035d976
Parker Hutcheson <pdhutcheson@gmail.com>
2023-06-18 02:22:27 -0400

    Added query search feature, series searches, and general cleaning

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -6,14 +6,45 @@ export const findAllMembers = async () => {
   return data;
 };
 
-export const searchForAMember = async (name) => {
-  if (name.length > 12) throw error();
+export const findSpecificMembersBasedOnCriteria = async (searchTerm) => {
   const data = await parseData('members');
-  
-  let result = 'Member not found';
-  data.forEach((member) => {
-    if (member['name'] === formattedName) result = member;
+  // Find a specific member
+  if (searchTerm.indexOf('+') > 0) {
+    const memberResults = [];
+    const parsedSearchTerm = await parseName(searchTerm);
+    data.forEach((member) => {
+      if (member.name === parsedSearchTerm) memberResults.push(member);
+    });
+    return memberResults;
+  }
+  // Find all members from a series
+  const seriesKeys = { 
+    'bxg': 'Boys x Girls Next Door',
+    'b&g': 'Boys & Girls in the City',
+    'aloha': 'Aloha State',
+    'ond': 'Opening New Doors',
+    'tokyo': 'Tokyo 2019-2020' 
+  };
+  if (searchTerm in seriesKeys) {
+    const chosenSeries = seriesKeys[searchTerm];
+    const seriesResults = [];
+    data.forEach((member) => {
+      if (member.series === chosenSeries) seriesResults.push(member);
+    });
+    return seriesResults;
+  }
+  // General Query
+  const lowercaseSearchTerm = searchTerm.toLowerCase();
+  const results = data.filter((object) => {
+    const lowercaseName = object.name.toLowerCase();
+    const lowercaseNickname = object.nickname.map((nickname) =>
+      nickname.toLowerCase()
+    );
+    return (
+      lowercaseName.includes(lowercaseSearchTerm) ||
+      lowercaseNickname.includes(lowercaseSearchTerm)
+    );
   });
-  return result;
+  return results;
 };
 
